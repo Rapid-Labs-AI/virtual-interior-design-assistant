56c49234fdab1cad3282d2685a79a462409849bb
ahmar-rapidlabs <ahmar@rapidlabs.ai>
2024-03-12 23:01:30 +0500

    Initial Commit

---
+++ src/components/nav.jsx
@@ -0,0 +1,123 @@
+import { useState } from 'react'
+import { Dialog} from '@headlessui/react'
+import { Link } from 'react-router-dom'
+import {
+  Bars3Icon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+
+
+export default function Nav() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  
+
+  return (
+    <header className="main_navbar">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <Link to="#" className="-m-1.5 p-1.5">
+            
+            <img className="h-8 w-auto" src={require('../logo.png')} alt="" />
+            <span className="sr-only">Company Name</span>
+
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+        <Link to="#" className="text-sm font-semibold leading-6 text-blue-900">
+            Home
+          </Link>
+          <Link to="#" className="text-sm font-semibold leading-6 text-blue-900">
+            About us
+          </Link>
+          <Link to="#" className="text-sm font-semibold leading-6 text-blue-900">
+            Features
+          </Link>
+          <Link to="#" className="text-sm font-semibold leading-6 text-blue-900">
+            Testimonials
+          </Link>
+          <Link to="#" className="text-sm font-semibold leading-6 text-blue-900">
+            FAQs
+          </Link>
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <Link to="/chat" className="text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-small rounded-lg text-sm px-5 py-2.5 dark:bg-blue-800 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Try our Chatbot
+          </Link>
+        </div>
+      </nav>
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <Link to="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img className="h-8 w-auto" src={require('../logo.png')} alt="" />
+            </Link>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+              <Link
+                  to="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Home
+                </Link>
+                <Link
+                  to="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  About us
+                </Link>
+                <Link
+                  to="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Features
+                </Link>
+                <Link
+                  to="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Testimonials
+                </Link>
+                <Link
+                  to="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  FAQs
+                </Link>
+              </div>
+              <div className="py-6">
+                <Link
+                  to="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Try our Chat Bot
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  )
+}
